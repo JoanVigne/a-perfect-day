a84a9aabe7a97277a8169dcbcdb332b3dbb3b059
Joan <joan.vigne@hotmail.fr>
2024-04-12 14:43:11 +0100

    more in form workout

--- src/app/workout/components/FormWorkout.tsx
+++ src/app/workout/components/FormWorkout.tsx
@@ -1,43 +1,84 @@
 "use client";
+import TemporaryMessage from "@/components/TemporaryMessage";
 import { fetchDataFromDBToLocalStorage } from "@/firebase/db/db";
 import React, { useState } from "react";
 
+interface Exercice {
+  name: string;
+  id: string;
+  description: string;
+  creationDate: string;
+}
+interface Workout {
+  name: string;
+  id: string;
+  description: string;
+  creationDate: string;
+  exercices: string[];
+}
 const FormWorkout = () => {
   const [exoFromDb, setExoFromDb] = useState({});
   // the workout shape :
   //workout =  randomid : { name, description, creation date, exercices: [id,id,id]. }
 
   async function fetchExoFromDb() {
-    // fetch the exercices from the db
-    const dataFromDb = await fetchDataFromDBToLocalStorage("exercices");
-    console.log(dataFromDb);
-
-    setExoFromDb(dataFromDb);
+    const data = await fetchDataFromDBToLocalStorage("exercices");
+    setExoFromDb(data);
   }
 
   // form :
-  function submitForm(e: React.FormEvent<HTMLFormElement>) {
+  const [messageForm, setMessageForm] = useState("");
+  const [workout, setWorkout] = useState({});
+  function submitRawWorkout(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-
     const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
     const name = formData.get("name");
+    if (!name) {
+      setMessageForm("Name is required");
+      return;
+    }
     const description = formData.get("description");
     const exercices = formData.getAll("exercices");
     const id = "workout" + Math.random().toString(36);
     console.log(name, id, description, exercices);
+    const rawWorkout = {
+      name: name,
+      id: id,
+      description: description,
+      exercices: exercices,
+    };
+    console.log("rawWorkout", rawWorkout);
+    setWorkout(rawWorkout);
   }
+  function submitWorkout(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("workout", workout);
+  }
+  // drag and drop :
+  let draggedExo: any;
+  function dragStart(event: React.DragEvent<HTMLDivElement>) {
+    draggedExo = event.currentTarget;
+  }
+  function dragOver(event: React.DragEvent<HTMLDivElement>) {
+    event.preventDefault();
+    const closestExo = event.currentTarget;
+    if (closestExo && closestExo.parentNode) {
+      closestExo.parentNode.insertBefore(draggedExo, closestExo);
+    }
+  }
+
   return (
     <div>
-      <form action="" onSubmit={submitForm}>
+      <form action="" onSubmit={submitRawWorkout}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" required />
         <label htmlFor="description">Description</label>
         <input type="text" name="description" id="description" />
-        <label htmlFor="exercices">Exercices</label>
+        <label htmlFor="exercices">Select exercices</label>
 
-        <button onClick={fetchExoFromDb}>
-          {exoFromDb ? "" : "See the exercices"}
+        <button type="button" onClick={fetchExoFromDb}>
+          {Object.values(exoFromDb).length > 0 ? "" : "See the exercices"}
         </button>
         <ul className="list-exo">
           {exoFromDb &&
@@ -55,8 +96,8 @@ const FormWorkout = () => {
               );
             })}
         </ul>
-
-        <button type="submit">Create workout</button>
+        <TemporaryMessage message={messageForm} timeInMS={2000} type="" />
+        <button type="submit">Continue</button>
       </form>
     </div>
   );
