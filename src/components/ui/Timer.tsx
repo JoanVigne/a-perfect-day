d600154fffab732f958b8f76d6fb8ff834fd734b
Joan <joan.vigne@hotmail.fr>
2024-05-08 11:03:42 +0100

    fix and improve Timer

--- src/components/ui/Timer.tsx
+++ src/components/ui/Timer.tsx
@@ -1,4 +1,140 @@
-import Icon from "@/components/ui/Icon";
+import React, { useState, useEffect } from "react";
+import Icon from "./Icon";
+
+interface Props {
+  timerValue: number;
+}
+
+const Timer: React.FC<Props> = ({ timerValue }) => {
+  const [seconds, setSeconds] = useState<number | null>(null);
+  const [isActive, setIsActive] = useState(false);
+  const [inputPlaceHolder, setInputPlaceHolder] = useState("1");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+  const minutes = seconds
+    ? Math.floor(seconds / 60)
+        .toString()
+        .padStart(2, "0")
+    : "00";
+
+  const remainingSeconds = seconds
+    ? (seconds % 60).toString().padStart(2, "0")
+    : "00";
+
+  useEffect(() => {
+    if (timerValue) {
+      const minutes = Math.floor(timerValue);
+      const seconds = Math.round((timerValue - minutes) * 100);
+      setSeconds(minutes * 60 + seconds);
+      setIsActive(true);
+    }
+  }, [timerValue]);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isActive && seconds !== null && seconds > 0) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) =>
+          prevSeconds !== null ? prevSeconds - 1 : null
+        );
+      }, 1000);
+    } else if (!isActive && seconds !== null) {
+      clearInterval(interval!);
+    }
+
+    return () => clearInterval(interval!);
+  }, [isActive, seconds]);
+  const handleReset = () => {
+    setIsActive(false);
+    setSeconds(null);
+  };
+  const handleStart = () => {
+    setIsActive(true);
+    if (inputValue) {
+      const minutes = Math.floor(parseFloat(inputValue));
+      const seconds = Math.round((parseFloat(inputValue) - minutes) * 100);
+      setSeconds(minutes * 60 + seconds);
+      console.log("minutes :", minutes, "seconds :", seconds);
+    } else {
+      const minutes = Math.floor(parseFloat(inputPlaceHolder));
+      const seconds = Math.round(
+        (parseFloat(inputPlaceHolder) - minutes) * 100
+      );
+      setSeconds(minutes * 60 + seconds);
+      console.log("minutes :", minutes, "seconds :", seconds);
+    }
+  };
+  function increment() {
+    setInputPlaceHolder((prevValue) => {
+      let newValue = parseFloat(prevValue) + 0.1;
+      let integerPart = Math.floor(newValue);
+      let decimalPart = newValue - integerPart;
+
+      if (decimalPart >= 0.6) {
+        integerPart += 1;
+        decimalPart = 0;
+      }
+      newValue = integerPart + decimalPart;
+      return newValue.toFixed(2);
+    });
+  }
+  function decrement() {
+    setInputPlaceHolder((prevValue) => {
+      let newValue = parseFloat(prevValue) - 0.1;
+      if (newValue <= 0) return "0";
+      let integerPart = Math.floor(newValue);
+      let decimalPart = newValue - integerPart;
+
+      if (decimalPart >= 0.6) {
+        decimalPart = 0.5;
+      }
+
+      newValue = integerPart + decimalPart;
+      return newValue.toFixed(2);
+    });
+  }
+  return (
+    <div className="timer">
+      <div className="time">
+        {!isActive ? (
+          <>
+            <div className="buttonsPlusAndMinus">
+              <button onClick={() => increment()}>+10s</button>
+              <button onClick={() => decrement()}>-10s</button>
+            </div>
+            <input
+              className="secondes"
+              type="number"
+              value={inputValue}
+              placeholder={inputPlaceHolder}
+              onChange={handleInputChange}
+            />
+          </>
+        ) : (
+          <div className="time-chrono">
+            {minutes}: {remainingSeconds}
+          </div>
+        )}
+      </div>
+      <div className="buttons">
+        <Icon nameImg="reset" onClick={handleReset} />
+        {!isActive && <Icon nameImg="play" onClick={handleStart} />}
+      </div>
+      {/* <div className="timer">
+      <div className="time">
+        {minutes}:{remainingSeconds}
+      </div>
+    </div> */}
+    </div>
+  );
+};
+
+export default Timer;
+/* import Icon from "@/components/ui/Icon";
 import React, { useState, useEffect } from "react";
 
 interface Props {
@@ -130,3 +266,4 @@ const Timer: React.FC<Props> = ({ timerValue }) => {
 };
 
 export default Timer;
+ */
