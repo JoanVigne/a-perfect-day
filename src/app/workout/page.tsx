3d4652d01ee8a1203c47a189131552dcd0790646
Joan <joan.vigne@hotmail.fr>
2024-05-08 15:03:06 +0100

    modal help

--- src/app/workout/page.tsx
+++ src/app/workout/page.tsx
@@ -8,6 +8,8 @@ import { fetchOnlyThisIdToLocalStorage } from "@/firebase/db/db";
 import { getItemFromLocalStorage } from "@/utils/localstorage";
 import CardWorkout from "../../components/cards/CardWorkout";
 import { useAuthContext } from "@/context/AuthContext";
+import Icon from "@/components/ui/Icon";
+import ModalHelp from "@/components/modals/ModalHelp";
 interface UserData {
   email: string;
   uid: string;
@@ -26,6 +28,7 @@ interface Workouts {
 export default function Page() {
   const { user } = useAuthContext() as { user: UserData };
   const [workouts, setWorkouts] = useState<Workouts>({});
+  const [openHelp, setOpenHelp] = useState(false);
   useEffect(() => {
     const fetchWorkouts = async () => {
       const localstorage = getItemFromLocalStorage("workouts");
@@ -45,7 +48,14 @@ export default function Page() {
   return (
     <>
       <Header />
-      <h1>Workout</h1>
+      <h1>
+        Workout <Icon nameImg="question" onClick={() => setOpenHelp(true)} />
+        <ModalHelp
+          isVisible={openHelp}
+          close={() => setOpenHelp(false)}
+          messagehelp="This is the workout page. You can create a new workout in the form. You can also click on the question mark of a workout to see its details and past performances"
+        />
+      </h1>
       <div className="container">
         {workouts && Object.values(workouts as Workouts).length > 0 ? (
           <>
---
+++ src/components/modals/ModalHelp.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactModal from "react-modal";
+
+interface Props {
+  isVisible: boolean;
+  close: () => void;
+  messagehelp: string;
+}
+const ModalHelp: React.FC<Props> = ({ isVisible, close, messagehelp }) => {
+  return (
+    <ReactModal
+      className="confirmModal"
+      isOpen={isVisible}
+      shouldCloseOnOverlayClick={true}
+      ariaHideApp={false}
+    >
+      <h2>Some explanations</h2>
+      <p>{messagehelp}</p>
+      <button onClick={close}>Okay</button>
+    </ReactModal>
+  );
+};
+
+export default ModalHelp;
