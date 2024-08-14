5734a6ee5177003c96d29e5173f75f43b5b324c6
Joan <joan.vigne@hotmail.fr>
2024-08-14 12:27:44 +0100

    css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import "./footer.css";
 import { usePathname } from "next/navigation";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   const pathname = usePathname();
@@ -15,37 +16,26 @@ const Footer = () => {
     typeof window !== "undefined" ? window.location.pathname : "";
   return (
     <footer>
+      {/*    <NavLink
+        to="/messages"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        Messages
+      </NavLink>
+       */}
       <nav>
         <ul>
-          <li>
-            <Link href="/" className={pathname == "/" ? "active-link" : ""}>
-              Routine
-            </Link>
+          <li className={pathname == "/" ? "active-link" : ""}>
+            <Link href="/">Routine</Link>
           </li>
-          <li>
-            <Link
-              href="/improve"
-              className={pathname == "/improve" ? "active-link" : ""}
-            >
-              Improve
-            </Link>
+          <li className={pathname == "/improve" ? "active-link" : ""}>
+            <Link href="/improve">Improve</Link>
           </li>
-          <li>
-            <Link
-              href="/workout"
-              className={pathname == "/workout" ? "active-link" : ""}
-            >
-              Workout
-            </Link>
+          <li className={pathname == "/workout" ? "active-link" : ""}>
+            <Link href="/workout">Workout</Link>
           </li>
-          {/*  <li>
-            <Link
-              href="/historic"
-              className={pathname == "/historic" ? "active-link" : ""}
-            >
-              Historic
-            </Link>
-          </li> */}
         </ul>
       </nav>
     </footer>
