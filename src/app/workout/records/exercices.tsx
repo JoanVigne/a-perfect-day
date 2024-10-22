815708b6e27d561c6c79d951214954eb9d59fa7a
Joan <joan.vigne@hotmail.fr>
2024-10-22 18:47:25 +0100

    fix record const

--- src/app/workout/records/exercices.tsx
+++ src/app/workout/records/exercices.tsx
@@ -1,81 +1,174 @@
 export const exerciseNameGroups = [
+  // Bench Press (Barbell)
   [
     "bench",
     "bench press",
     "benchpress",
+    "bench barre",
     "bp",
     "développé couché barre",
     "développé couché",
+    "dév couché",
     "dc",
   ],
-  ["squat", "squats"],
+  // Incline Bench Press (Barbell)
+  [
+    "incline bench press",
+    "incline bench",
+    "incline bench barre",
+    "incline bp",
+    "développé incliné barre",
+  ],
+  // Decline Bench Press (Barbell)
+  [
+    "decline bench press",
+    "decline bench",
+    "decline bench barre",
+    "decline bp",
+    "développé décliné barre",
+  ],
+  // Dumbbell Bench Press
+  [
+    "dumbbell bench press",
+    "dumbbellbenchpress",
+    "dbbp",
+    "dumbbell press",
+    "dumbbell chest press",
+  ],
+  // Dumbbell Incline Bench Press
+  [
+    "dumbbell incline bench press",
+    "dumbbell incline bench",
+    "dumbbell incline dbbp",
+  ],
+  // Dumbbell Decline Bench Press
+  ["dumbbell decline bench press", "dumbbell decline bench"],
+  // Squat (Barbell)
+  ["squat", "squats", "back squat", "front squat", "bs", "fs", "squat barre"],
+  // Goblet Squat (Dumbbell)
+  ["goblet squat", "gs", "dumbbell squat", "dumbbell squats"],
+  // Overhead Press (Barbell)
   [
     "overhead press",
     "overheadpress",
     "overhead press barre",
     "overheadpress barre",
     "développé militaire barre",
+    "military press",
+    "militarypress",
     "ohp",
   ],
+  // Overhead Press (Dumbbell)
   [
-    "développé militaire dumbell",
+    "dumbbell overhead press",
+    "dumbbell overheadpress",
+    "dumbbell military press",
     "développé militaire haltères",
     "développé militaire haltère",
-    "overheadpress dumbell",
-    "overhead press dumbell",
-    "overheadpress dumbell",
-    "overheadpress haltères",
-    "overhead press haltères",
-    "overheadpress haltères",
-    "overheadpress haltère",
-    "overhead press haltère",
-    "overheadpress haltère",
-    "ohp",
+    "ohp dumbbell",
   ],
+  // Deadlift (Barbell)
   [
     "deadlift",
     "dead lift",
     "dl",
     "soulevé de terre",
     "deadlift barre",
-    "dead lift barre",
-    "dl barre",
     "soulevé de terre barre",
+    "sumo deadlift",
+    "romanian deadlift",
+    "rdl",
+    "conventional deadlift",
   ],
-  ["shoulder press", "shoulderpress", "sp"],
-  ["pull-up", "pullup", "pull up"],
-  ["dip", "dips"],
-  ["curl"],
-  ["tricep extension", "tricepextension", "tricep ext"],
-  ["leg press", "legpress", "lp"],
-  ["leg curl", "legcurl", "lc"],
-  ["leg extension", "legextension", "le"],
-  ["leg raise", "legraise", "lr"],
-  ["calf raise", "calfraise", "cr"],
-  ["lateral raise", "lateralraise", "lr"],
-  ["front raise", "frontraise", "fr"],
-  ["rear delt raise", "reardeltraise", "rdr"],
-  ["shrug"],
-  ["bent-over row", "bentoverrow", "bor"],
-  ["row"],
-  ["pull-over", "pullover", "po"],
-  ["pulldown", "pull down", "pd"],
-  ["lat pulldown", "latpulldown", "lat pd"],
-  ["seated row", "seatedrow", "sr"],
-  ["face pull", "facepull", "fp"],
-  ["reverse fly", "reversefly", "rf"],
-  ["chest fly", "chestfly", "cf"],
-  ["pec fly", "pecfly", "pf"],
-  ["cable fly", "cablefly", "cf"],
+  // Shoulder Press (Machine or Barbell)
+  [
+    "shoulder press",
+    "shoulderpress",
+    "sp",
+    "seated shoulder press",
+    "machine shoulder press",
+  ],
+  // Dumbbell Shoulder Press
+  [
+    "dumbbell shoulder press",
+    "dumbbell overhead press",
+    "db shoulder press",
+    "dumbbell sp",
+  ],
+  // Pull-ups and Variations
+  ["pull-up", "pullup", "pull up", "chin-up", "chinup", "chin up"],
+  // Dips and Variations
+  ["dip", "dips", "parallel bar dips", "bench dips"],
+  // Bicep Curl (Barbell)
+  ["barbell curl", "ez curl", "bicep curl", "preacher curl", "curl"],
+  // Dumbbell Curl
+  ["dumbbell curl", "dumbbell bicep curl", "dbc", "alternating dumbbell curl"],
+  // Tricep Extension (Barbell or Machine)
+  [
+    "tricep extension",
+    "tricepextension",
+    "tricep ext",
+    "overhead tricep extension",
+    "machine tricep extension",
+  ],
+  // Dumbbell Tricep Extension
+  [
+    "dumbbell tricep extension",
+    "dumbbell overhead tricep extension",
+    "dbte",
+    "dumbbell skullcrusher",
+  ],
+  // Leg Press (Machine)
+  ["leg press", "legpress", "lp", "machine leg press"],
+  // Leg Curl (Machine)
+  ["leg curl", "legcurl", "lc", "seated leg curl", "lying leg curl"],
+  // Leg Extension (Machine)
+  ["leg extension", "legextension", "le", "quad extension"],
+  // Leg Raise (Bodyweight)
+  ["leg raise", "legraise", "lr", "hanging leg raise"],
+  // Calf Raise
+  ["calf raise", "calfraise", "cr", "standing calf raise", "seated calf raise"],
+  // Lateral Raise (Dumbbell)
+  [
+    "lateral raise",
+    "lateralraise",
+    "lr",
+    "dumbbell lateral raise",
+    "side lateral raise",
+  ],
+  // Front Raise (Dumbbell)
+  ["front raise", "frontraise", "fr", "dumbbell front raise"],
+  // Rear Delt Raise (Dumbbell)
+  [
+    "rear delt raise",
+    "reardeltraise",
+    "rdr",
+    "dumbbell rear delt raise",
+    "reverse fly",
+    "reverse pec fly",
+  ],
+  // Shrug (Barbell or Dumbbell)
+  ["shrug", "barbell shrug", "dumbbell shrug"],
+  // Bent-over Row (Barbell)
+  ["bent-over row", "bentoverrow", "bor", "barbell row"],
+  // Dumbbell Row
+  ["dumbbell row", "dumbbell one-arm row", "db row", "dbr"],
+  // Pullover (Dumbbell)
+  ["dumbbell pullover", "db pullover", "dumbbell pull-over"],
+  // Lat Pulldown (Machine)
+  ["lat pulldown", "lat pull down", "pd", "pulldown", "lat pd"],
+  // Seated Row (Machine)
+  ["seated row", "seatedrow", "sr", "machine row"],
+  // Face Pull (Cable)
+  ["face pull", "facepull", "fp", "cable face pull"],
+  // Cable Fly
+  ["cable fly", "cablefly", "cf", "cable chest fly"],
+  // Cable Crossover
   ["cable crossover", "cablecrossover", "cc"],
-  ["push-up", "pushup", "push up", "pompe", "pompes"],
-  ["dumbbell press", "dumbbellpress", "dbp"],
-  ["dumbbell fly", "dumbbellfly", "dbf"],
-  ["dumbbell bench press", "dumbbellbenchpress", "dbbp"],
-  ["dumbbell curl", "dumbbellcurl", "dbc"],
-  ["dumbbell tricep extension", "dumbbelltricepextension", "dbte"],
-  ["dumbbell row", "dumbbellrow", "dbr"],
-  ["dumbbell pullover", "dumbbellpullover", "dbpo"],
-  ["dumbbell lateral raise", "dumbbelllateralraise", "dblr"],
-  ["dumbbell front raise", "dumbbellfrontraise", "dbfr"],
+  // Push-ups and Variations
+  ["push-up", "pushup", "push up", "pompe", "pompes", "wide push-up"],
+  // Dumbbell Press
+  ["dumbbell press", "db chest press", "dbp"],
+  // Dumbbell Fly
+  ["dumbbell fly", "db fly", "dumbbell chest fly", "dbf"],
 ];
